c7fd3597a99e7bc3123d56f2b66faa7208e84a8f
chokrihamza <chokrihamza1994@gmail.com>
2021-08-16 12:55:52 +0200

    -construct fn

---
+++ datatypes.py
@@ -0,0 +1,32 @@
+x = 5
+print(type(x))
+x = "hello there"
+print(type(x))
+x = 1+1j
+print(type(x))
+x = ["a", "b", "c", "d"]
+print(type(x))
+x = ("a", "b", "c", "d")
+print(type(x))
+x = range(10)
+print(type(x))
+x = {
+    "name": "hamza",
+    "age": 20,
+    "hobbies": ['maths', 'development', 'listen to music']
+}
+print(type(x))
+x = {1, 2, 3, 4, 5}
+print(type(x))
+x = frozenset({1, 2, 3, 4, 5})
+print(type(x))
+x = True
+print(type(x))
+x = False
+print(type(x))
+x = b'hello'
+print(type(x))
+x = bytearray(10)
+print(type(x))
+x = memoryview(bytes(10))
+print(type(x))
