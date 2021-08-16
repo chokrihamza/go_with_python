6b1834fd781a68faeb76c2e99986d119f6604cc1
chokrihamza <chokrihamza1994@gmail.com>
2021-08-16 17:53:04 +0200

    work with booleans

---
+++ booleans.py
@@ -0,0 +1,14 @@
+print(10>2)
+print(10<2)
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
+if 10<2:
+  print("ten is grower then two")
+else:
+  print('not good ')
