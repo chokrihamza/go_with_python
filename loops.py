5b3fca4d70673048bce5b08f7b4fad55b0f13ac9
chokrihamza <chokrihamza1994@gmail.com>
2021-08-19 12:43:40 +0200

    work with conditions  in python

---
+++ loops.py
@@ -0,0 +1,17 @@
+i = 0
+while i < 6:
+  i += 1 # here it work so goood
+  if i == 3:
+    continue
+  #i += 1 # attention process not killed
+  print(i)
+else:
+  print("end")
+  
+  
+# for loop
+
+for i in "hello":
+  print(i)
+else:
+  print("end")
