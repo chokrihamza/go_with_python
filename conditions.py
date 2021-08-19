93d641a118e011175d50cbb88e6bece67c89829d
chokrihamza <chokrihamza1994@gmail.com>
2021-08-19 12:10:42 +0200

    work with conditions  in python

---
+++ conditions.py
@@ -0,0 +1,19 @@
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+  
+# short hand if
+
+if 5<1: print("yesss")
+
+# ternary operators
+a=5
+b=5
+print("A") if a>b else print("B") if a<b else print("=")
+if a>b:
+  pass
